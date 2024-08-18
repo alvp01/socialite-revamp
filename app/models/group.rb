3a7da2fe013573c563cce20bf84215fdf2963181
Abel Lavieri <abel.lavieri@gmail.com>
2024-08-18 14:38:28 -0400

    Add group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+  belongs_to :admin, class_name: 'User', foreign_key: 'admin_id'
+  has_many :group_memberships
+  has_many :members, through: :group_memberships, source: :user
+  has_many :posts
 end
