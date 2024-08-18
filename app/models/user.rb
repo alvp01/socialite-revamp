050ef2f76db4e2c51ed5cc934beca17e902edd34
Abel Lavieri <abel.lavieri@gmail.com>
2024-08-18 14:40:57 -0400

    Add user follow relation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+  validates :username, :email, uniqueness: true
+  validates :username, :email, :name, :lastname, presence: true
+
+  has_many :posts
+
+  has_many :active_follows, class_name: 'UserFollow', foreign_key: 'follower_id', dependent: :destroy
+  has_many :following, through: :active_follows, source: :followed
+
+  has_many :passive_follows, class_name: 'UserFollow', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followers, through: :passive_follows, source: :follower
+
   has_many :posts
 end
